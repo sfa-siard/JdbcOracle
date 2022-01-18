87c34b04fb3d965091e5c5b4e17ee3343e769f6c
Max Burri <burri@puzzle.ch>
2022-01-18 14:25:53 +0100

    build: fix typo in image tag name

--- docker/build-oracle-image.sh
+++ docker/build-oracle-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://github.com/oracle/docker-images.git
 cd docker-images/OracleDatabase/SingleInstance/dockerfiles
-./buildContainerImage.sh -v 18.4.0 -x -t oracled
+./buildContainerImage.sh -v 18.4.0 -x -t oracledb
 
 cd -
 pwd
