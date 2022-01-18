abf60167634cf425bff20890fa91b7703867bf6a
Max Burri <burri@puzzle.ch>
2022-01-18 14:10:13 +0100

    build: clone oracle docker images repo with https

--- docker/build-oracle-image.sh
+++ docker/build-oracle-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone git@github.com:oracle/docker-images.git
+git clone https://github.com/oracle/docker-images.git
 cd docker-images/OracleDatabase/SingleInstance/dockerfiles
 ./buildContainerImage.sh -v 18.4.0 -x -t oracled
 
