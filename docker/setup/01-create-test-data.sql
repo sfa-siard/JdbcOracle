c7066fdcdd3a12d18344429370de21ad410ed9fb
Max Burri <burri@puzzle.ch>
2022-01-20 09:37:04 +0100

    fix: add buguser
    undocumented user buguser used in ch.admin.bar.siard2.jdbc.OracleStatementTester.testExecuteBug and ch.admin.bar.siard2.jdbc.OracleStatementTester.testDownloadLongRaw

--- docker/setup/01-create-test-data.sql
+++ docker/setup/01-create-test-data.sql
@@ -6,3 +6,9 @@ CREATE USER TEST_SCHEMA identified by testschemapwd;
 -- specific user for ch.admin.bar.siard2.jdbc.OracleDatabaseMetaDataTester.testGetOeTables and ch.admin.bar.siard2.jdbc.OracleDatabaseMetaDataTester.testGetOeTables
 CREATE USER OE identified by OEPWD;
 GRANT CONNECT, RESOURCE TO OE;
+
+
+-- specific undocumented user "buguser" used in ch.admin.bar.siard2.jdbc.OracleStatementTester.testExecuteBug and ch.admin.bar.siard2.jdbc.OracleStatementTester.testDownloadLongRaw
+CREATE USER buguser identified by bugpwd;
+GRANT CONNECT, RESOURCE TO buguser;
+GRANT UNLIMITED TABLESPACE TO buguser;
