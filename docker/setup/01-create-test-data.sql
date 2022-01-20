c9cb7297886687ca51aa00c45432c95b1d7950e9
Max Burri <burri@puzzle.ch>
2022-01-20 09:25:42 +0100

    fix: add user OE
    undocumented user OE is used in tests ch.admin.bar.siard2.jdbc.OracleDatabaseMetaDataTester.testGetOeTables and ch.admin.bar.siard2.jdbc.OracleDatabaseMetaDataTester.testGetOeTables

--- docker/setup/01-create-test-data.sql
+++ docker/setup/01-create-test-data.sql
@@ -3,3 +3,6 @@ ALTER SESSION SET CONTAINER=XEPDB1;
 -- OracleConnectionTesters needs a schema "TEST_SCHEMA" - create the user (oracle treats users as schemas?)
 CREATE USER TEST_SCHEMA identified by testschemapwd;
 
+-- specific user for ch.admin.bar.siard2.jdbc.OracleDatabaseMetaDataTester.testGetOeTables and ch.admin.bar.siard2.jdbc.OracleDatabaseMetaDataTester.testGetOeTables
+CREATE USER OE identified by OEPWD;
+GRANT CONNECT, RESOURCE TO OE;
