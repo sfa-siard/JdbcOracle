787dea39697dafa144a6107855c9bf48a8ec96ff
llorentelemmc <llorente@puzzle.ch>
2025-05-19 15:32:52 +0200

    Setup Gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+/*
+ * Build file for JdbcOracle Gradle build.
+ *
+ * This file configures the build for the JdbcOracle project.
+ */
+
+
+plugins {
+    `java-library`
+    id("pl.allegro.tech.build.axion-release") version "1.14.3"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+version = scmVersion.version
+
+repositories {
+    mavenCentral()
+    flatDir {
+        dirs("lib")
+    }
+}
+
+dependencies {
+    implementation("org.antlr:antlr4-runtime:4.5.2")
+
+    implementation(fileTree("lib") { include("*.jar") })
+
+    testImplementation("junit:junit:4.13.1")
+    testImplementation("org.hamcrest:hamcrest-core:1.3")
+
+    //necessary for useJUnitPlatform()
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.vintage:junit-vintage-engine")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "jdbcoracle"
+include("lib")
