dacf51b3d3e5d6a002f5cc6e824370ff9afb5c2f
llorentelemmc <llorente@puzzle.ch>
2025-08-18 10:33:58 +0200

    chore: migrate to Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,11 @@ plugins {
     id("io.freefair.lombok") version "6.5.0"
 }
 
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 group = "ch.admin.bar"
